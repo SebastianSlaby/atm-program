4f75eb8ff5877bcdc119f339dfa070f20e6a9b75
Sebastian Słaby <sebastianslaby98@gmail.com>
2019-02-25 20:16:11 +0100

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->pinInput->setAlignment(Qt::AlignCenter);
+    QFont pinFont = ui->pinInput->font();
+    pinFont.setPointSize(32);
+    ui->pinInput->setFont(pinFont);
+    //linking signals to slots
+    connect(ui->pinSubmitButton,SIGNAL(clicked()),this,SLOT(checkPin()));
+    //
+    ui->pinInput->setFocus();
+    ui->pinInput->setInputMask("9999");
+    ui->pinInput->setEchoMode(QLineEdit::Password);
+    QShortcut *shortcut = new QShortcut(QKeySequence("Enter"),ui->pinSubmitButton);
+    connect(shortcut,SIGNAL(activated()),this,SLOT(checkPin()));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::checkPin(){
+    if(ui->pinInput->text()=="2137"){
+        delete ui->pinInput;
+        delete ui->pinSubmitButton;
+        ui->withdrawButton->setEnabled(true);
+        ui->depositButton->setEnabled(true);
+    }
+
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QShortcut>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void checkPin();
+};
+
+#endif // MAINWINDOW_H
