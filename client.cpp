5aa6ff686e982382cf6315b233f520a9dcc5c20e
Sebastian Słaby <sebastianslaby98@gmail.com>
2019-02-26 09:41:12 +0100

    ui improvements, client class

--- client.cpp
+++ client.cpp
@@ -1,6 +1,15 @@
 #include "client.h"
 
-client::client()
+client::client(int currentBalance, QString fullName)
 {
+    this->currentBalance = currentBalance;
+    this->fullName = fullName;
+}
 
+QString client::getFullName(){
+    return this->fullName;
+}
+
+int client::getCurrentBalance(){
+    return this->currentBalance;
 }
